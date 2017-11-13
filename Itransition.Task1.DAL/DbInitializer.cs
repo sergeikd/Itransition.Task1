1fa3427ab950d80313f01a47c849a99158e72f91
ITRANSITION\s2.kudryavtsev <s2.kudryavtsev@itransition.com>
2017-11-13 11:52:13 +0300

    DB initialization with some default users

---
+++ Itransition.Task1.DAL/DbInitializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.Entity;
+using Itransition.Task1.Domain;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+
+namespace Itransition.Task1.DAL
+{
+    public class DbInitializer : DropCreateDatabaseAlways<AppDbContext>
+    {
+        protected override void Seed(AppDbContext context)
+        {
+            var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+
+            var userList = new[] {
+                            new { Name = "qqq@qqq.qq", Password = "qqqqqq" }, 
+                            new { Name = "aaa@aaa.aa", Password = "aaaaaa" },
+                            new { Name = "zzz@zzz.zz", Password = "zzzzzz" },
+            };
+            foreach (var user in userList)
+            {
+                var isentityUser = new ApplicationUser            
+                {
+                    Email = user.Name,
+                    UserName = user.Name,
+                    EmailConfirmed = true
+                };
+                userManager.Create(isentityUser, user.Password);
+                isentityUser.BankAccount = new BankAccount { AccountNumber = Guid.NewGuid().ToString().ToUpper(), Amount = 0 };
+            }
+            context.SaveChanges();
+        }
+    }
+}
