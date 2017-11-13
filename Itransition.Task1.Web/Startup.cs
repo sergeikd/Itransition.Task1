042437aeb9014f8330f58c93d55a490abb2121cf
ITRANSITION\s2.kudryavtsev <s2.kudryavtsev@itransition.com>
2017-11-13 11:00:14 +0300

    Default MVC5 + Identity template

---
+++ Itransition.Task1.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Itransition.Task1.Web.Startup))]
+namespace Itransition.Task1.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
